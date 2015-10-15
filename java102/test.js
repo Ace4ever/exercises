e11b673168243b1e9376fd4227f12c48a02e5687
Christina <c_j_y@ymail.com>
2015-10-15 15:45:38 -0500

    java102

---
+++ java102/test.js
@@ -0,0 +1,79 @@
+// var fruit = ["cherries", "apples", "bananas"];
+// console.log("fruit", fruit);
+// var fruitString = fruit.join(","); // "cherries,apples,bananas"
+// fruitString = fruit.join(", "); // "cherries apples bananas"
+// console.log("fruitString", fruitString);
+
+// var fruits = "watermelon|lemon|orange";
+// var fruitArray = fruits.split("|");
+// console.log("fruitArray", fruitArray);
+
+// var fruit = ["cherries", "apples", "bananas"];
+// var backwards_fruit = fruit.reverse(); // ["bananas", "apples", "cherries"]
+// console.log ("backwards_fruit", backwards_fruit);
+
+// var fruit = ["cherries", "apples", "bananas"];
+// var sorted_fruit = fruit.sort(); // ["apples", "bananas", "cherries"]
+// console.log("sorted_fruit", sorted_fruit);
+
+// Objects
+
+var car = {
+	make: "Nissan",
+	wheels: 4,
+	model: "Xterra"
+// 	options {
+// 		ac: true, 
+// 		bulletProof: false,
+// 		underlighting: false,
+// 		spinners: true
+// 	}
+// };
+
+console.log(car.options.spinners);
+
+// Adding
+
+// function add(one, two) {
+// 	console.log(one, two);
+// 	return one + two;
+// }
+
+// var result = add(1, 2);
+// console.log(result);
+
+// Calling functions inside functions
+
+// var globalVar = 90
+
+// function add(cheetah, dolphin) {
+// 	return dolphin (cheetah, 3);
+// }
+
+// function subtract(one, two) {
+// 	return one - two;
+// }
+
+// var result = add(1, subtract);
+// console.log("result", result);
+
+// Division
+
+// var globalVar = 90
+
+// function add(cheetah, dolphin) {
+// 	return dolphin + cheetah;
+// }
+
+// function subtract(one, two) {
+// 	return one - two;
+// }
+
+// function divide(one, two) {
+// 	return one/two;
+// }
+
+// var result1 = add(1, 2);
+// var result2 = subtract(10, 2);
+// var result3 = divide(10, 2);
+// console.log(result1, result2, result3);
