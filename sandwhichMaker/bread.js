28b9c9ba888062dd02f1d61c46db3f368d794a5d
Christina <c_j_y@ymail.com>
2015-10-30 16:33:01 -0500

    makin sandwhiches

---
+++ sandwhichMaker/bread.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+var Sandwhich = (function(breadType) {
+    var bread = $("#bread").click().val();
+    console.log(bread)
+  
+    breadType.getBreadType = function() {
+  
+  //     var stringDisplay = "" ;
+  //   for (i=0; i<bread.length; i++){
+  
+  //     var currentInput = userInputArray[i];
+  //     // displayMessage = french[currentInput];
+  //     stringDisplay += french[currentInput] + " ";
+  //     $("#container").html(stringDisplay);
+  
+  //     }
+  //   return french[userInput];
+  //   }
+
+  //   sandwhich.getBread = function(){
+  //     return bread;
+    }
+  // return sandwhich;
+
+  })
+(Sandwhich);
+});
+// console.log(solarSystem);
+// console.log(solarSystem.setAge(9));
+  // All the JavaScript that depends on jQuery will be written
+
